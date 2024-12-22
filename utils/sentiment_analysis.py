662680ed66df3455b2b87aabd79ca1258a0aeac8
Anshidtp <shaanshid07@gmail.com>
2024-12-22 19:26:42 +0530

    vader sentiment analyzer file added

---
+++ utils/sentiment_analysis.py
@@ -0,0 +1,12 @@
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+analyzer = SentimentIntensityAnalyzer()
+
+def analyze_sentiment(text):
+    scores = analyzer.polarity_scores(text)
+    if scores['compound'] >= 0.05:
+        return "Positive"
+    elif scores['compound'] <= -0.05:
+        return "Negative"
+    else:
+        return "Neutral"
